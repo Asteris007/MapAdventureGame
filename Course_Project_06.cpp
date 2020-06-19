65b50dd6dc0fa7e4a355b16bf3d2c323e8bad03c
Asteris007 <62188477+Asteris007@users.noreply.github.com>
2020-06-19 15:48:09 +0300

    Update Course_Project_06.cpp

--- Course_Project_06.cpp
+++ Course_Project_06.cpp
@@ -1,14 +1,16 @@
-﻿
+
 #include "theMap.h"
 #include <iostream>
 
 int main()
 {
-	theMap m;
+	Character* ch = new Character("name", 0, 1, 2, 3, 4, 1, 1);
+	theMap m{ 1, 1, ch };
 	m.fullscreen();
 	m.moveChar();
 	
 return 0;
 
 }
+
 
