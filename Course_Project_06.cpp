74f2e44b5500c17921da78f6258e45089159528e
Asteris007 <62188477+Asteris007@users.noreply.github.com>
2020-06-23 17:09:07 +0300

    Update Course_Project_06.cpp

--- Course_Project_06.cpp
+++ Course_Project_06.cpp
@@ -1,16 +1,24 @@
 
 #include "theMap.h"
+#include "TheMagician.h"
 #include <iostream>
+#include <string>
+
+using std::string;
 
 int main()
 {
-	Character* ch = new Character("name", 0, 1, 2, 3, 4, 1, 1);
-	theMap m{ 1, 1, ch };
+	//Character* ch = new Character("name", 0, 1, 2, 3, 4, 1, 1);
+	//theMap m{ 1, 1, ch };
+	
+	TheMagician* mag = new TheMagician("Magic", 100, 40, 40, 1, 1, 60);
+	mag->DisplayM();
+
+	theMap m{ 1,1,mag };
 	m.fullscreen();
 	m.moveChar();
 	
 return 0;
 
 }
-
 
