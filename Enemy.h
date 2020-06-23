9ebeafadaaa125b791d5433b836326b9ce35c0ce
Asteris007 <62188477+Asteris007@users.noreply.github.com>
2020-06-23 17:09:59 +0300

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -4,10 +4,31 @@
 
 class Enemy
 {
-private:
-	 
+public:
+	int e_hp;
+	int e_strenght;
+	int e_intelligence;
+	int x, y;
 
 
+	Enemy();
+	~Enemy();
+	Enemy(int e_hp, int e_strenght, int e_intelligence, int x, int y);
+	void setEHP(int e_hp);
+	void setEStren(int e_strenght);
+	void setEIntell(int e_intelligence);
+	void setEX(int x);
+	void setEY(int y);
+
+	int getEHP();
+	int getESren();
+	int getEIntell();
+	int getEX();
+	int getEY();
+
+	virtual void EAttack(int e_hp, int e_strenght, int e_intelligence);
+	virtual void EDefend(int e_hp, int e_strenght, int e_intelligence);
+	
 
 };
 
